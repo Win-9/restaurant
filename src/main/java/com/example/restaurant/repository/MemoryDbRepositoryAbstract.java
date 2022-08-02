d70f3217b2a99c45dc58f4e8adc1371b7c9a4eda
Win_9 <krrr883@gmail.com>
2022-08-02 23:32:26 +0900

    add Repository

---
+++ src/main/java/com/example/restaurant/repository/MemoryDbRepositoryAbstract.java
@@ -0,0 +1,50 @@
+package com.example.restaurant.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+abstract public class MemoryDbRepositoryAbstract<T extends MemoryDbEntity> implements MemoryDbRepositoryIfs<T>{
+
+    private final List<T> db = new ArrayList<>();
+    private int index = 0;
+
+    @Override
+    public Optional<T> findById(int index) {
+        return db.stream().filter(it -> it.getIndex() == index).findFirst();
+    }
+
+    @Override
+    public T save(T entity) {
+        Optional<T> optionalEntity = db.stream().filter(it -> it.getIndex() == entity.getIndex()).findFirst();
+
+        if (optionalEntity.isEmpty()) {//db에 값이 없는경우
+            index++;
+            entity.setIndex(index);
+            db.add(entity);
+
+            return entity;
+        }
+        else {
+            int index = optionalEntity.get().getIndex();
+            entity.setIndex(index);
+
+            deleteById(index);
+            db.add(entity);
+            return entity;
+        }
+    }
+
+    @Override
+    public void deleteById(int index) {
+        Optional<T> optionalEntity = db.stream().filter(it -> it.getIndex() == index).findFirst();
+        if (optionalEntity.isPresent()){
+            db.remove(optionalEntity.get());
+        }
+    }
+
+    @Override
+    public List<T> listAll() {
+        return db;
+    }
+}
---
+++ src/main/java/com/example/restaurant/repository/wishlist/repository/WishListRepository.java
@@ -0,0 +1,11 @@
+package com.example.restaurant.repository.wishlist.repository;
+
+import com.example.restaurant.repository.MemoryDbRepositoryAbstract;
+import com.example.restaurant.repository.wishlist.entity.WishListEntity;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class WishListRepository extends MemoryDbRepositoryAbstract<WishListEntity> {
+
+}
